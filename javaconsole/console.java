8e09fb2823d9351abcc22f8742690db50042ec33
GabbyLo <35555422+GabbyLo@users.noreply.github.com>
2019-03-05 17:48:54 +0800

    3

--- javaconsole/console.java
+++ javaconsole/console.java
@@ -5,16 +5,30 @@ class mainconsole{
 	public static void main(String[] args) {
 		grade12class obj = new grade12class();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a String value: ");
-		String thevalue = sc.nextLine();
-		obj.getvalue(thevalue);
+		// System.out.println("Enter a String value: ");
+		// String thevalue = sc.nextLine();
+		// obj.getvalue(thevalue);
+		System.out.print("Enter Username: ");
+		String xusername=sc.nextLine();
+		System.out.print("Enter Password: ");
+		String xpassword=sc.nextLine();
+
 
 	}
 }
 
 class grade12class
 {
-	
+	private String username ="codeus";
+	private String password ="1234";
+	public String getusername()
+	{
+		return username;
+	}
+	public String getpassword()
+	{
+		return password;
+	}
 	public void getvalue(String x)
 	{
 		System.out.println("The value is "+x);
