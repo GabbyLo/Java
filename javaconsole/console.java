d07676f606eef2f656b2aafc931e3795347db773
GabbyLo <35555422+GabbyLo@users.noreply.github.com>
2019-03-05 17:33:41 +0800

    1

---
+++ javaconsole/console.java
@@ -0,0 +1,17 @@
+
+class mainconsole{
+
+	public static void main(String[] args) {
+		System.out.println("Main Console Test");
+		grade12class objg12class =new grade12class();
+		objg12class.displaymessage();
+	}
+}
+
+class grade12class
+{
+	public void displaymessage()
+	{
+		System.out.println("Hello this is a message from grade 12 class");
+	}
+}
